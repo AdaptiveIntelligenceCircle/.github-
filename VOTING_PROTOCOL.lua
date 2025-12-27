09426fb6e5a2feacc18188237883b494c4117548
ndtribk <ndtribk@gmail.com>
2025-12-28 00:44:36 +0700

    VOTING_PROTOCOL.lua

--- VOTING_PROTOCOL.lua
+++ VOTING_PROTOCOL.lua
@@ -3,3 +3,6 @@
 -- This module defines a local agreement mechanism
 -- for system components, not for human governance.
 -- It is designed for internal adaptive coordination.
+
+local lfs = require("lfs") -- LuaFileSystem for directory iteration (optional)
+-- If lfs not available, we'll use io.popen fallbacks for listing
